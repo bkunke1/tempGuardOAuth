c0d8722591292993618801d781c20ba8ee1eb66a
Brandon Kunkel <brandon.lloyd.kunkel@gmail.com>
2021-04-26 00:31:31 -0400

    update ui for production

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ mongoose.connect(
 
 // Middle
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://musing-golick-d5c510.netlify.app', credentials: true }));
 
 app.set('trust proxy', 1)
 
@@ -159,7 +159,7 @@ app.get(
   passport.authenticate('google', { failureRedirect: '/login' }),
   function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect('http://localhost:3000');
+    res.redirect('https://musing-golick-d5c510.netlify.app');
   }
 );
 
@@ -170,7 +170,7 @@ app.get(
   passport.authenticate('twitter', { failureRedirect: '/login' }),
   function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect('http://localhost:3000');
+    res.redirect('https://musing-golick-d5c510.netlify.app');
   }
 );
 
@@ -181,7 +181,7 @@ app.get(
   passport.authenticate('github', { failureRedirect: '/login' }),
   function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect('http://localhost:3000');
+    res.redirect('https://musing-golick-d5c510.netlify.app');
   }
 );
 
